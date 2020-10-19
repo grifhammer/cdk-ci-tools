b9a29c327b5e7682e850f4594c160e34f21bb306
Griffin Hammer <grifhammer@gmail.com>
2020-10-18 23:15:02 -0700

    First commit

--- lib/cdk-ci-tools-stack.ts
+++ lib/cdk-ci-tools-stack.ts
@@ -1,9 +1,46 @@
-import * as cdk from '@aws-cdk/core';
-
+import * as cdk from "@aws-cdk/core";
+import { Pipeline, Artifact } from "@aws-cdk/aws-codepipeline";
+import { PipelineProject } from "@aws-cdk/aws-codebuild";
+import {
+  GitHubSourceAction,
+  CodeBuildAction,
+} from "@aws-cdk/aws-codepipeline-actions";
 export class CdkCiToolsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const sourceOutput = new Artifact();
+    const sourceAction = new GitHubSourceAction({
+      actionName: "GitHub_Source",
+      owner: "grifhammer",
+      repo: "cdk-ci-tools",
+      oauthToken: cdk.SecretValue.secretsManager("GithubAccessToken"),
+      output: sourceOutput,
+      branch: "develop", // default: 'master'
+    });
+
+    const project = new PipelineProject(this, "CDKPipelineBuilder");
+
+    const codebuildAction = new CodeBuildAction({
+      input: sourceOutput,
+      actionName: "cdkBuild",
+      project,
+    });
+
+    const pipeline = new Pipeline(this, "Pipeline", {
+      pipelineName: "SetlistPipeline",
+      stages: [
+        {
+          stageName: "source",
+          actions: [sourceAction],
+        },
+        {
+          stageName: "build",
+          actions: [codebuildAction],
+        },
+      ],
+    });
+    // pipeline.addStage({ stageName: "test", actions: [codebuildAction] });
   }
 }
--- test/cdk-ci-tools.test.ts
+++ test/cdk-ci-tools.test.ts
@@ -1,13 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as CdkCiTools from '../lib/cdk-ci-tools-stack';
+import {
+  expect as expectCDK,
+  matchTemplate,
+  MatchStyle,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as CdkCiTools from "../lib/cdk-ci-tools-stack";
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkCiTools.CdkCiToolsStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+test("Empty Stack", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkCiTools.CdkCiToolsStack(app, "MyTestStack");
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
